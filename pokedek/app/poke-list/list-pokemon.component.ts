ef4287c9ff418c3a7621ff44ab1333e2ddf00c0b
Russell Frame <rframe83@gmail.com>
2017-10-12 12:18:18 -0700

    Pokedek: Jumbotron

--- pokedek/app/poke-list/list-pokemon.component.ts
+++ pokedek/app/poke-list/list-pokemon.component.ts
@@ -1,16 +1,25 @@
 
 
 import {Component, OnInit} from "@angular/core";
+import {PokemonService} from "../shared/pokemon.service";
+import {Pokemon} from "../shared/pokemon";
 
 @Component({
+    selector: 'pk-list',
     moduleId: 'ListPokemonComponent',
     templateUrl: '/app/poke-list/list-pokemon.template.html'
 })
 
 export class ListPokemonComponent implements OnInit {
-    constructor() {}
+    public pokemon: Pokemon[];
+    public errorMessage: string;
+    constructor(private _pokemonService: PokemonService) {}
 
     ngOnInit() {
+    // Get All Pokemon
+        this.getPokemons();
+    }
+    getPokemons() {
 
     }
 }
